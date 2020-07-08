bf30780aa544e315bc3424281502b68118e8987a
Farel Escalante <farel_92@hotmail.com>
2020-07-07 18:32:03 -0600

    agregando ejercicio 5

---
+++ Ejercicios/Ejercicio5-Programa Suma/programa-suma.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+
+using namespace std;
+
+int main(int argc, char const *argv[])
+{
+    int a = 0;
+    int b = 0;
+    int resultado = 0;
+
+    cout<<"Ingrese el valor de a: ";
+    cin>>a;
+
+    cout<<endl;
+
+    cout <<"Ingrese el valor de b: ";
+    cin >>b;
+
+
+    resultado = a + b;
+
+    cout << "La suma de a + b es: "  << resultado;
+
+
+    return 0;
+}
