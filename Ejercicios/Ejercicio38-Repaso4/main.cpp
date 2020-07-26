e46e582e180f4d19495163a6c68ac92249a4102e
Farel Escalante <farel_92@hotmail.com>
2020-07-26 00:32:51 -0600

    agregando examen primer parcial

---
+++ Ejercicios/Ejercicio38-Repaso4/main.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+using namespace std;
+
+int main(int argc, char const *argv[])
+{
+    while (/* condition */)
+    {
+        /* code */
+    }
+    
+    
+
+    return 0;
+}
