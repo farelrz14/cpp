6fc7d834ed4f137fd9b96128a50f11d046df1cf5
Farel Escalante <farel_92@hotmail.com>
2020-08-02 21:56:26 -0600

    agregando ejercicio de punto de venta con impuesto

--- Ejercicios/Ejercicio44-Punto-de-Venta-V/factura.cpp
+++ Ejercicios/Ejercicio44-Punto-de-Venta-V/factura.cpp
@@ -3,12 +3,16 @@
 using namespace std;
 
 double subtotal;
+double impuesto;
+double total;
 string listaProductos;
 
 void agregarProducto(string descripcion, int cantidad, double precio)
 {
     listaProductos = listaProductos + descripcion + '\n';
     subtotal = subtotal + (cantidad * precio);
+    impuesto = subtotal * 0.15;
+    total = subtotal + impuesto;
 }
 
 void imprimirFactura()
@@ -23,7 +27,9 @@ void imprimirFactura()
     cout << listaProductos;
 
     cout << endl;
-    cout << "Subtotal: " << subtotal;
+    cout << "Subtotal: " << subtotal << endl;
+    cout << "impuesto: " << impuesto << endl;
+    cout << "total a pagar: " << total;
     cout << endl;
     cout << endl;
     system("pause");
--- Ejercicios/Ejercicio44-Punto-de-Venta-V/productos.cpp
+++ Ejercicios/Ejercicio44-Punto-de-Venta-V/productos.cpp
@@ -18,6 +18,7 @@ void productos(int opcion)
        cout << "*****************" << endl; 
        cout << "1 - Capuccino" << endl; 
        cout << "2 - Expresso" << endl;
+       cout << "3 - Chocolate Caliente" << endl;
        cout << endl;
 
        cout << "Ingrese una opcion: ";
@@ -30,6 +31,9 @@ void productos(int opcion)
             break;
         case 2:
             agregarProducto("1 Expresso - L30.00", 1, 30); 
+            break;
+        case 3:
+            agregarProducto("1 Chocolate caliente - L30.00", 1, 30); 
             break;
         default:
            {
@@ -49,14 +53,74 @@ void productos(int opcion)
     {
         cout << "BEBIDAS FRIAS" << endl;
         cout << "*************" << endl;
-        system("pause");
+        cout << "1 - Granita de Cafe" << endl; 
+        cout << "2 - Te frio" << endl;
+        cout << "3 - Granita de Bandera" << endl;
+        cout << endl;
+
+        cout << "Ingrese una opcion: ";
+        cin >> opcionProducto;
+
+        switch (opcionProducto)
+        {
+        case 1:
+            agregarProducto("1 Granita de Cafe - L35.00", 1, 35); 
+            break;
+        case 2:
+            agregarProducto("1 Te frio - L35.00", 1, 35); 
+            break;
+        case 3:
+            agregarProducto("1 Granita de Bandera - L35.00", 1, 35); 
+            break;
+        default:
+           {
+             cout <<"opcion no valida";
+             return;    
+             break;
+           } 
+        }
+        
+        cout << endl;
+        cout << "Producto agregado" << endl << endl;
+        system ("pause");
+        
         break;
     }
     case 3:
     {
         cout << "REPOSTERIA" << endl;
         cout << "**********" << endl;
-        system("pause");
+        cout << "1 - Chilena" << endl; 
+        cout << "2 - Galleta de Mantequilla" << endl;
+        cout << "3 - Pastel de Chocolate" << endl;
+        cout << endl;
+
+        cout << "Ingrese una opcion: ";
+        cin >> opcionProducto;
+        
+        switch (opcionProducto)
+        {
+        case 1:
+            agregarProducto("1 Chilena - L25.00", 1, 25); 
+            break;
+        case 2:
+            agregarProducto("1 Galleta de Mantequilla - L20.00", 1, 20); 
+            break;
+        case 3:
+            agregarProducto("1 Pastel de Chocolate - L25.00", 1, 25); 
+            break;
+        default:
+           {
+             cout <<"opcion no valida";
+             return;    
+             break;
+           } 
+        }
+        
+        cout << endl;
+        cout << "Producto agregado" << endl << endl;
+        system ("pause");
+
         break;
     }
     default:
